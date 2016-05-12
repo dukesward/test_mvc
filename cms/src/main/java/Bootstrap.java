3224bb8e5d41b8b34786aa6dfb3b29a53afb6c4b
Chen <206457840@tfayd.com>
2016-05-12 16:57:08 -0400

    add files to cms folder

--- cms/src/main/java/Bootstrap.java
+++
@@ -1,8 +0,0 @@
-package bootstrap;
-
-public class Bootstrap {
-	
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-}
---
+++ cms/src/main/java/com/test/cms/kingdom/web/BootstrapInitializer.java
@@ -0,0 +1,19 @@
+package com.test.cms.kingdom;
+
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class BootstrapInitializer implements WebApplicationInitializer {
+	
+	@Override
+	public void onStartup(ServletContext context) throws ServletException {
+		System.out.println("hello");
+		ServletRegistration.Dynamic registration = context.addServlet("dispatcher", new DispatcherServlet());
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/cms/*");
+	}
+}
