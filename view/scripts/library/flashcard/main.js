36df925eb6e4651a762389a7a638ada0a6d26767
Chen <206457840@tfayd.com>
2016-06-17 14:49:53 -0400

    more on flashcard service library

--- view/scripts/library/flashcard/main.js
+++ view/scripts/library/flashcard/main.js
@@ -3,5 +3,13 @@ if(typeof jQuery === 'function' && FlashCard) {
 		var flashCard = new FlashCard();
 
 		flashCard._service.get();
+		injectWordCards(flashCard._service._card);
+
+		function injectWordCards(data) {
+			var $container = $('.card-container');
+
+			
+		}
+
 	})(jQuery);
 }
